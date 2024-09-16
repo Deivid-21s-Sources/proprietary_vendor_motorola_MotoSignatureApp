9a499aad02ff78414239103b70b90b4aba53d4fe
Deivid21 <david.parra.ignacio@gmail.com>
2024-09-15 23:54:17 -0300

    MotoSignatureApp: makefiles
    Change-Id: I25d3146b2e09b2957d01200c5aaf1769756f5389

--- motosignatureapp.mk
+++ motosignatureapp.mk
@@ -1,5 +1,20 @@
-# Automatically generated file. DO NOT MODIFY
+#
+# Copyright (C) 2008 - 2024 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
+# SOONG NAMESPACES
 PRODUCT_SOONG_NAMESPACES += \
     vendor/motorola/MotoSignatureApp
 
@@ -7,23 +22,44 @@ PRODUCT_SOONG_NAMESPACES += \
 BOARD_PLAT_PRIVATE_SEPOLICY_DIRS += \
     vendor/motorola/MotoSignatureApp/sepolicy/private
 
+# Moto Signature App
+# Version: 1.0
 PRODUCT_COPY_FILES += \
-    vendor/motorola/MotoSignatureApp/proprietary/system/bin/dropboxhelper:$(TARGET_COPY_OUT_SYSTEM)/bin/dropboxhelper \
+    vendor/motorola/MotoSignatureApp/proprietary/system/etc/permissions/com.motorola.motosignature.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.motorola.motosignature.xml
+
+PRODUCT_PACKAGES += \
+    com.motorola.motosignature \
+    MotoSignatureApp
+
+# Moto Signature 2 App
+# Version: 1.0
+PRODUCT_PACKAGES += \
+    MotoSignature2App
+
+# Moto Settings
+PRODUCT_COPY_FILES += \
     vendor/motorola/MotoSignatureApp/proprietary/system/etc/permissions/com.motorola.frameworks.core.addon.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.motorola.frameworks.core.addon.xml \
-    vendor/motorola/MotoSignatureApp/proprietary/system/etc/permissions/com.motorola.motosignature.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.motorola.motosignature.xml \
     vendor/motorola/MotoSignatureApp/proprietary/system/etc/permissions/moto-checkin.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/moto-checkin.xml \
     vendor/motorola/MotoSignatureApp/proprietary/system/etc/permissions/moto-core_services.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/moto-core_services.xml \
     vendor/motorola/MotoSignatureApp/proprietary/system/etc/permissions/moto-settings.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/moto-settings.xml \
-    vendor/motorola/MotoSignatureApp/proprietary/system_ext/bin/motsettings:$(TARGET_COPY_OUT_SYSTEM_EXT)/bin/motsettings \
+    vendor/motorola/MotoSignatureApp/proprietary/system_ext/bin/motsettings:$(TARGET_COPY_OUT_SYSTEM_EXT)/bin/motsettings
+
+PRODUCT_PACKAGES += \
+    com.motorola.frameworks.core.addon \
+    moto-checkin \
+    moto-core_services \
+    moto-settings
+
+# Motorola Dropbox Helper
+PRODUCT_COPY_FILES += \
+    vendor/motorola/MotoSignatureApp/proprietary/system/bin/dropboxhelper:$(TARGET_COPY_OUT_SYSTEM)/bin/dropboxhelper
+
+PRODUCT_PACKAGES += \
+    com.motorola.android.dropboxhelper
+
+# Public Libraries
+PRODUCT_COPY_FILES += \
     vendor/motorola/MotoSignatureApp/proprietary/system_ext/etc/public.libraries-moto.txt:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/public.libraries-moto.txt
 
 PRODUCT_PACKAGES += \
-    com.motorola.android.dropboxhelper \
-    com.motorola.frameworks.core.addon \
-    com.motorola.motosignature \
-    libenginepostproc.moto \
-    moto-checkin \
-    moto-core_services \
-    moto-settings \
-    MotoSignatureApp \
-    MotoSignature2App
+    libenginepostproc.moto
